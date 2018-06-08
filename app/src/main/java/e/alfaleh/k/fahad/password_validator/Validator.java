1ba934378a9d177454124df84504a5eb024f9005
Fahadzz4 <fahad.k.alfaleh@gmail.com>
2018-06-07 21:13:08 -0300

    stage 2 done

--- app/src/main/java/e/alfaleh/k/fahad/password_validator/Validator.java
+++ app/src/main/java/e/alfaleh/k/fahad/password_validator/Validator.java
@@ -1,13 +1,31 @@
 package e.alfaleh.k.fahad.password_validator;
 
-public class Validator {
+import java.util.regex.Pattern;
 
+public class Validator {
+//some code was taken from https://stackoverflow.com/questions/36097097/password-validate-8-digits-contains-upper-lowercase-and-a-special-character
     public int valid(String t){
+        Pattern specailCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Pattern UpperCasePatten = Pattern.compile("[A-Z]");
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+
         int strength=0;
+
         if(t.length()>=8)
             strength++;
+
         if(!t.equals("password"))
             strength++;
+
+        if (specailCharPatten.matcher(t).find())
+            strength++;
+
+        if (UpperCasePatten.matcher(t).find())
+            strength++;
+
+        if (digitCasePatten.matcher(t).find())
+            strength++;
+
         return strength;
     }
 }
--- app/src/test/java/e/alfaleh/k/fahad/password_validator/ValidatorUnitTest.java
+++ app/src/test/java/e/alfaleh/k/fahad/password_validator/ValidatorUnitTest.java
@@ -9,25 +9,32 @@ public class ValidatorUnitTest {
     static String t1="IloveMyself";
     static String t2="password";
     static String t3="cool";
+    static String t4="cool!12";
+    static String t5="PassWo--rdIsCool!";
     static Validator validator;
     @BeforeClass
     public static void testing(){
         validator= new Validator();
     }
-
     @Test
     public void t1Valid(){
 
-        assertEquals(2,validator.valid(t1));
+        assertEquals(3,validator.valid(t1));
     }
-
     @Test
     public void t2Valid(){
-        assertEquals(2,validator.valid(t2));
+        assertEquals(1,validator.valid(t2));
     }
-
     @Test
     public void t3Valid(){
-        assertEquals(2,validator.valid(t3));
+        assertEquals(1,validator.valid(t3));
+    }
+    @Test
+    public void t4Valid(){
+        assertEquals(4,validator.valid(t4));
+    }
+    @Test
+    public void t5Valid(){
+        assertEquals(5,validator.valid(t5));
     }
 }
